951ff2258569d0fcd5a6bb10b0c0a6ac7cb77556
Boatman, Tommy <Tommy_Boatman@bmc.com>
2018-11-05 09:05:24 -0600

    C# Deploy Azure VM from template example from Microsoft.
    TODO: Customize the generic parameters

---
+++ vmDeployment/vmDeployment/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.Compute.Fluent;
+using Microsoft.Azure.Management.Compute.Fluent.Models;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace vmDeployment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the management client
+
+            var credentials = SdkContext.AzureCredentialsFactory
+                .FromFile(Environment.GetEnvironmentVariable("AZURE_AUTH_LOCATION"));
+
+            var azure = Azure
+                .Configure()
+                .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+                .Authenticate(credentials)
+                .WithDefaultSubscription();
+
+            // Create a resource group
+
+            var groupName = "myResourceGroup";
+            var location = Region.USWest;
+
+            var resourceGroup = azure.ResourceGroups.Define(groupName)
+                .WithRegion(location)
+                .Create();
+
+            // Create a storage account
+
+            string storageAccountName = SdkContext.RandomResourceName("st", 10);
+
+            Console.WriteLine("Creating storage account...");
+            var storage = azure.StorageAccounts.Define(storageAccountName)
+                .WithRegion(Region.USWest)
+                .WithExistingResourceGroup(resourceGroup)
+                .Create();
+
+            var storageKeys = storage.GetKeys();
+            string storageConnectionString = "DefaultEndpointsProtocol=https;"
+                + "AccountName=" + storage.Name
+                + ";AccountKey=" + storageKeys[0].Value
+                + ";EndpointSuffix=core.windows.net";
+
+            var account = CloudStorageAccount.Parse(storageConnectionString);
+            var serviceClient = account.CreateCloudBlobClient();
+
+            Console.WriteLine("Creating container...");
+            var container = serviceClient.GetContainerReference("templates");
+            container.CreateIfNotExistsAsync().Wait();
+            var containerPermissions = new BlobContainerPermissions()
+            { PublicAccess = BlobContainerPublicAccessType.Container };
+            container.SetPermissionsAsync(containerPermissions).Wait();
+
+            Console.WriteLine("Uploading template file...");
+            var templateblob = container.GetBlockBlobReference("CreateVMTemplate.json");
+            templateblob.UploadFromFileAsync("..\\..\\CreateVMTemplate.json").Result();
+
+            Console.WriteLine("Uploading parameters file...");
+            var paramblob = container.GetBlockBlobReference("Parameters.json");
+            paramblob.UploadFromFileAsync("..\\..\\Parameters.json").Result();
+
+            // Deploy a template
+
+            var templatePath = "https://" + storageAccountName + ".blob.core.windows.net/templates/CreateVMTemplate.json";
+            var paramPath = "https://" + storageAccountName + ".blob.core.windows.net/templates/Parameters.json";
+            var deployment = azure.Deployments.Define("myDeployment")
+                .WithExistingResourceGroup(groupName)
+                .WithTemplateLink(templatePath, "1.0.0.0")
+                .WithParametersLink(paramPath, "1.0.0.0")
+                .WithMode(Microsoft.Azure.Management.ResourceManager.Fluent.Models.DeploymentMode.Incremental)
+                .Create();
+            Console.WriteLine("Press enter to delete the resource group...");
+            Console.ReadLine();
+
+            // Delete the resources 
+
+            azure.ResourceGroups.DeleteByName(groupName);
+
+        }
+    }
+}
